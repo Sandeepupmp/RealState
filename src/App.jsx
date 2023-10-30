95d85cf551a76efeabb9edd35dd36f4a4aefbe07
sandeep <sandeepupmp@gmail.com>
2023-10-30 22:31:01 +0530

    add react router dom for routing and page foler

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
 
 function App() {
   
 
   return (
-    <>
-      <h1 className='bg-red-500 border-lime-200'>hello sir</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
