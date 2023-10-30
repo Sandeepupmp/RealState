128797d544ab53913075a60f2658c2058f76ea03
sandeep <sandeepupmp@gmail.com>
2023-10-30 21:19:26 +0530

    hello this is first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-red-500 border-lime-200'>hello sir</h1>
+    </>
+  )
+}
+
+export default App
